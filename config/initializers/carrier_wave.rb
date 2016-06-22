8c458963c1f846410f5abc9db98949c90fd7889c
Rails Udemy course <railsudemycourse@gmail.com>
2016-06-22 07:40:53 +0000

    error

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      #:region => ENV['S3_REGION']
+      :region => 'eu-west-1'
       }
     config.fog_directory = ENV['S3_BUCKET']
-    config.cache_dir     = "#{Rails.root}/tmp/uploads"
+    #config.cache_dir     = "#{Rails.root}/tmp/uploads"
   end
 end
